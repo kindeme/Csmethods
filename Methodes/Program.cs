3151108047413d22d994ae412492714f0cb8eccd
Kindeme <fkounoudji@gmail.com>
2020-02-27 19:12:42 -0600

    Add project files.

---
+++ Methodes/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methodes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer C1 = new Customer("flore", "xevosa");
+            C1.PrintFullName();
+            Console.ReadLine();
+        }
+        
+    }
+}
+class Customer
+{
+    string firstName;
+    string lastName;
+    // cConstructor with default values
+    public Customer()
+        : this("No first NAme provided", "No lastname provided")
+    {
+    }
+
+    //Constructor
+    public Customer(string FirstName, string LastName)
+    {
+        this.firstName = FirstName;
+        this.lastName = LastName;
+    }
+    public void PrintFullName()
+    {
+        Console.WriteLine("Full Name = {0}", this.firstName + " " + this.lastName);
+    }
+    ~Customer()
+    {
+        // clean up code
+    }
+}
---
+++ Staticand Instance/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Staticand_Instance
+{
+    class Circle
+    {
+        float PI = 3.141F;
+        int Radius;
+        public Circle(int radius)
+        {
+            this.Radius = radius;
+        }
+        public float CalculateArea()
+        {
+            return this.PI * this.Radius * this.Radius;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Circle C1 = new Circle(5);
+            float Area = C1.CalculateArea();
+            Console.WriteLine("Area = {0}", Area);
+            Console.ReadLine();
+        }
+        
+    }
+}
